c3b0502eecbbd0ace204442fe0a56e0ecd249063
Hasthi <hardstudio.rambukwella@gmail.com>
2023-11-01 23:10:17 +0530

    install shadcn

---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {Button} from "@/components/ui/button";
+
+const Nav = () => {
+    return (
+        <header>
+            <nav>
+                <Button>{"Shadcn button"}</Button>
+            </nav>
+        </header>
+    );
+};
+
+export default Nav;
