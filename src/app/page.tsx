9079882ef04159af1f32542254d02542b276bb79
Hasthi <hardstudio.rambukwella@gmail.com>
2023-11-17 00:04:40 +0530

    add tiptap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Nav from "@/components/Nav";
+import Editor from "@/components/Editor";
 
 export default function Home() {
   return (
-    <main className="flex">
-      <Nav />
+    <main className="flex p-5">
+      {/*<Nav />*/}
+      <Editor />
     </main>
   )
 }
---
+++ src/components/Editor.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useEditor, EditorContent, type Editor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import Placeholder from '@tiptap/extension-placeholder'
+import Toolbar from "@/components/Toolbar";
+
+const Editor = () => {
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+            Placeholder,
+        ],
+        editorProps: {
+            attributes: {
+                class: "min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            }
+        },
+        onUpdate({editor}){
+            console.log(editor.getHTML())
+        }
+    })
+
+    return (
+        <div>
+            <Toolbar editor={editor} />
+            <EditorContent editor={editor} />
+        </div>
+    )
+};
+
+export default Editor;
