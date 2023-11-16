bf309a6314ef531a3bd16253113765bfeebadbc5
Hasthi <hardstudio.rambukwella@gmail.com>
2023-11-17 00:15:14 +0530

    add minor fix

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -6,14 +6,14 @@ import Placeholder from '@tiptap/extension-placeholder'
 import Toolbar from "@/components/Toolbar";
 
 const Editor = () => {
-    const editor = useEditor({
+    const editor: Editor | null = useEditor({
         extensions: [
             StarterKit,
             Placeholder,
         ],
         editorProps: {
             attributes: {
-                class: "min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                class: "min-h-[80px] max-w-[300px] rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
             }
         },
         onUpdate({editor}){
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -15,35 +15,35 @@ const Toolbar = ({ editor }: ToolbarProps) => {
             <Toggle
                 size='sm'
                 pressed={editor.isActive("heading", { level: 2})}
-                onPressedChange={() => editor?.chain().focus().toggleHeading({ level: 2 }).run()}
+                onPressedChange={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
             >
                 <Heading1 className={'h-4 w-4'}/>
             </Toggle>
             <Toggle
                 size='sm'
                 pressed={editor.isActive("bold")}
-                onPressedChange={() => editor?.chain().focus().toggleBold().run()}
+                onPressedChange={() => editor.chain().focus().toggleBold().run()}
             >
                 <Bold className={'h-4 w-4'}/>
             </Toggle>
             <Toggle
                 size='sm'
                 pressed={editor.isActive("italic")}
-                onPressedChange={() => editor?.chain().focus().toggleItalic().run()}
+                onPressedChange={() => editor.chain().focus().toggleItalic().run()}
             >
                 <Italic className={'h-4 w-4'}/>
             </Toggle>
             <Toggle
                 size='sm'
                 pressed={editor.isActive("strike")}
-                onPressedChange={() => editor?.chain().focus().toggleStrike().run()}
+                onPressedChange={() => editor.chain().focus().toggleStrike().run()}
             >
                 <Strikethrough className={'h-4 w-4'}/>
             </Toggle>
             <Toggle
                 size='sm'
                 pressed={editor.isActive("code")}
-                onPressedChange={() => editor?.chain().focus().toggleCode().run()}
+                onPressedChange={() => editor.chain().focus().toggleCode().run()}
             >
                 <Code className={'h-4 w-4'}/>
             </Toggle>
